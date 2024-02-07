722f0042ef8d2c107a218b7a8c28017402716cd2
cuevacelis <cuevacelis@hotmail.com>
2024-02-06 22:21:36 -0500

    feat: add fetch table

---
+++ app/prueba-tecnica/myTable.tsx
@@ -0,0 +1,11 @@
+import NavComponent from "./components/nav/nav";
+import Table from "./components/table/table";
+
+export default function MyTable(props: any) {
+  return (
+    <>
+      <NavComponent />
+      <Table />
+    </>
+  );
+}
