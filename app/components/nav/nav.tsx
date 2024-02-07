527008de0bc53a0f74b73e6ec1dfa09c297c22e4
cuevacelis <cuevacelis@hotmail.com>
2024-02-07 13:18:25 -0500

    fix: add optimize img logo

--- app/components/nav/nav.tsx
+++ app/components/nav/nav.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 
@@ -7,11 +8,12 @@ export default function Nav() {
     <Navbar bg="dark" expand="lg" data-bs-theme="dark">
       <Container fluid>
         <Navbar.Brand className="mx-auto" href="/">
-          <img
+          <Image
             src="https://logo.clearbit.com/clearbit.com"
             alt="Logo de la empresa"
             className="d-inline-block align-text-top"
-            height="30"
+            width={30}
+            height={30}
           />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
